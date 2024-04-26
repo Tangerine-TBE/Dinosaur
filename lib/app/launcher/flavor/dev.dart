b7130083954181482af8327e29f8f5e4d1cd8ae4
谭伯恩 <tanboen@cxw-chip.com>
2024-04-26 17:41:20 +0800

    闪屏图

--- lib/app/launcher/flavor/dev.dart
+++ lib/app/launcher/flavor/dev.dart
@@ -8,7 +8,6 @@ import '../strategy/dev_launcher_strategy.dart';
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await Future.delayed(Duration(seconds: 3));
   FlutterNativeSplash.remove();
   AppLauncher.launch(DevLauncherStrategy());
 }
